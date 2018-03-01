323e00400bb159c8d561db85445cbb62dbcba12d
NegMawon <ingkid@icloud.com>
2018-02-28 16:07:04 -0800

    routes updatede

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'post#index'
+  # with the resources line, the 7 controller actions are created:
+  # get (new, :id, posts), patch or put and delete
+  resources :posts
 end
